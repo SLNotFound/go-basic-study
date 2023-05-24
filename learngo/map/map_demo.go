2ba89ea77c4c2f9c77a784b0b2e2e1aa8db56bc1
sunl <1255193087@qq.com>
2023-05-24 17:05:37 +0800

    add file

---
+++ learngo/map/map_demo.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	dict1 := make(map[string]int)
+	dict2 := map[string]string{"name": "sunlei", "age": "12"}
+	fmt.Println(dict1)
+	fmt.Println(dict2)
+}
